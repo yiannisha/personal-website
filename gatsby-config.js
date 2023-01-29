6834ee60316870d7dfa6d5ea6436cedd432f31b4
yiannisha <hadjiyiannis.3@gmail.com>
2023-01-29 23:11:53 +0200

    Set up Contentful CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,25 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Yiannis Hadjiyiannis | Portfolio Website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
